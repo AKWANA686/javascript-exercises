8d235c928f6d1b55d77163249cbc4c1bb89dccde
Akwana686 <akwanaoliver@gmail.com>
2025-01-16 17:21:07 +0300

    Update sum all integers between two positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return Error("Both numbers must be positive integers,");
+    }
 
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+    
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
