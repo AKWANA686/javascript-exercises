c9f85fe54acc6b1d5b0aaad4fe942951824f7cfd
Akwana686 <akwanaoliver@gmail.com>
2025-01-28 13:46:51 +0300

    Exercise 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,24 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
 const power = function() {
-	
+	return a ** b
 };
-
 const factorial = function() {
-	
+	return a % b
 };
 
 // Do not edit below this line
