da13645eff7d46c613cc9ec80ed4423e1d52c0bc
Akwana686 <akwanaoliver@gmail.com>
2025-01-28 14:07:31 +0300

    Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+    // Compare the cleaned string with its reverse
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
