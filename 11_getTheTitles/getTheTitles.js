3baa13fb5cd0eddbea94246ed8bfad85f010965a
Akwana686 <akwanaoliver@gmail.com>
2025-01-28 17:15:55 +0300

    Exercise 11 - Get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function (array) {
+    const titles = [];
+    for (const book of array) {
+        titles.push(book.title);
+    }
+    return titles;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
