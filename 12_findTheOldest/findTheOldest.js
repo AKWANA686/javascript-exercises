c382a767dfef12e5b291bc50e2319d6326117431
Akwana686 <akwanaoliver@gmail.com>
2025-01-29 11:42:46 +0300

    Exercise 12 - Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        const birthYear = person.yearOfBirth;
+        const deathYear = person.yearOfDeath || currentYear;
+
+        const age = deathYear - birthYear;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
